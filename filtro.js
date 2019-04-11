43b2bb60a0216d1e8d3c4eaf85ceaa8592b9e2b4
Leyre Millana Caballero <lmillana@beta28.aulas.gsyc.urjc.es>
2019-04-11 09:58:34 +0200

    Umbral de color rojo

--- filtro.js
+++ filtro.js
@@ -8,6 +8,12 @@ function main() {
   //-- Acceso al objeto con el canvas
   var canvas = document.getElementById('display');
 
+  //-- Acceso al deslizador
+  deslizador = document.getElementById('deslizador')
+
+  //-- Valor del deslizador
+  range_value = document.getElementById('range_value')
+
   //-- Se establece como tamaño del canvas el mismo
   //-- que el de la imagen original
   canvas.width = img.width;
@@ -21,48 +27,32 @@ function main() {
   //-- No se han hecho manipulaciones todavia
   ctx.drawImage(img, 0,0);
 
-  //-- Obtener la imagen del canvas en pixeles
-  var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  //-- Funcion de retrollamada del deslizador
+  deslizador.oninput = () => {
+    //-- Mostrar el nuevo valor del deslizador
+    range_value.innerHTML = deslizador.value
 
-  //-- Obtener el array con todos los píxeles
-  var data = imgData.data
+    //-- Situar la imagen original en el canvas
+    //-- No se han hecho manipulaciones todavia
+    ctx.drawImage(img, 0,0);
 
-  //-- data[0] es el canal rojo del pixel de la posición 0,0
-  //-- data[1] es el canal verde del pixel 0,0
-  //-- data[2] es el canal azul del pixel 0,0
-  //-- data[3] es el canal de transparencia del pixel 0,0
-  //-- data[4] es el canal ROJO del pixel 1,0
-  //-- ....
-  //-- En general, para el pixel i
-  //-- data[4*i] es el canal rojo
-  //-- data[4*i + 1]: Canal verde
-  //-- data[4*i + 2]: Canal azul
-  //-- data[4*i + 3]: Canal de transparencia
+    //-- Obtener la imagen del canvas en pixeles
+    var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
-  //-- Colocar un pixel rojo en (200,50)
+    //-- Obtener el array con todos los píxeles
+    var data = imgData.data
 
-  //-- Obtener el numero de pixel a partir de su posicion
-  var i = 200 + 50*canvas.width;
+    //-- Obtener el umbral de rojo del desliador
+    umbral = deslizador.value
 
-  //-- Pixel rojo: canal rojo a tope. Resto de colores a 0
-  //-- La transparencia no se modifica
-  data[i*4] = 255;    //-- Canal Rojo
-  data[i*4 + 1] = 0;  //-- Canal Verde
-  data[i*4 + 2] = 0;  //-- Canal azul
+    //-- Filtrar la imagen según el nuevo umbral
+    for (var i = 0; i < data.length; i+=4) {
+      if (data[i] > umbral)
+        data[i] = umbral;
+    }
 
-  //-- Poner la imagen modificada en el canvas
-  ctx.putImageData(imgData, 0, 0);
+    //-- Poner la imagen modificada en el canvas
+    ctx.putImageData(imgData, 0, 0);
+  }
 
-  /*-- Obtener el numero total de elementos en el array
-  console.log("Tamaño de data: " + data.length)
-
-  //-- El número total de pixeles es la altura por la anchura
-  npixels = canvas.width * canvas.height
-  console.log("Anchura (en pixeles): " + canvas.width)
-  console.log("Altura (en pixeles): " + canvas.height)
-  console.log("Pixeles totales: " + npixels)
-
-  //-- Puesto que cada pixel ocupa 4 bytes, el array de píxeles
-  //-- tiene un tamaño de 4 * numero de pixeles
-  console.log("Total de datos de la imagen: " + npixels * 4) */
 }
