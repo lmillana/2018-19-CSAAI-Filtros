a9dad3a59950d6d866a945923a89527d431e99e7
Leyree <leyremc@MacBook-Pro-de-Leyree.local>
2019-04-16 10:58:40 +0200

    Casi terminado -- falta estilo y retoques!

--- filtro.js
+++ filtro.js
@@ -8,11 +8,18 @@ function main() {
   //-- Acceso al objeto con el canvas
   var canvas = document.getElementById('display');
 
-  //-- Acceso al deslizador
-  deslizador = document.getElementById('deslizador')
+  //-- Acceso a los deslizadores: R,G,B
+  deslizador_R = document.getElementById('deslizador_R')  //-- ROJO
+  deslizador_G = document.getElementById('deslizador_G')  //-- VERDE
+  deslizador_B = document.getElementById('deslizador_B')  //-- AZUL
 
-  //-- Valor del deslizador
-  range_value = document.getElementById('range_value')
+  //-- Valor de los deslizadores:
+  range_value_R = document.getElementById('range_value_R')  //-- ROJO
+  range_value_G = document.getElementById('range_value_G')  //-- VERDE
+  range_value_B = document.getElementById('range_value_B')  //-- AZUL
+
+  //-- Botón para el cambio a escala de grises:
+  gris = document.getElementById('gris')
 
   //-- Se establece como tamaño del canvas el mismo
   //-- que el de la imagen original
@@ -27,10 +34,11 @@ function main() {
   //-- No se han hecho manipulaciones todavia
   ctx.drawImage(img, 0,0);
 
-  //-- Funcion de retrollamada del deslizador
-  deslizador.oninput = () => {
-    //-- Mostrar el nuevo valor del deslizador
-    range_value.innerHTML = deslizador.value
+  function toRGB() {
+    // -- Mostrar el nuevo valor del deslizador:
+    range_value_R.innerHTML = deslizador_R.value
+    range_value_G.innerHTML = deslizador_G.value
+    range_value_B.innerHTML = deslizador_B.value
 
     //-- Situar la imagen original en el canvas
     //-- No se han hecho manipulaciones todavia
@@ -42,16 +50,59 @@ function main() {
     //-- Obtener el array con todos los píxeles
     var data = imgData.data
 
-    //-- Obtener el umbral de rojo del desliador
-    umbral = deslizador.value
+    //-- Obtener el umbral según el desliador
+    umbral_R = deslizador_R.value
+    umbral_G = deslizador_G.value
+    umbral_B = deslizador_B.value
 
-    //-- Filtrar la imagen según el nuevo umbral
+    //-- Filtrar la imagen según el umbral ROJO:
     for (var i = 0; i < data.length; i+=4) {
-      if (data[i] > umbral)
-        data[i] = umbral;
+      if (data[i] > umbral_R)
+        data[i] = umbral_R;
     }
+    //-- Filtrar la imagen según el umbral VERDE:
+    for (var i = 0; i < data.length; i+=4) {
+      if (data[i] > umbral_G)
+        data[i] = umbral_G;
+    }
+    //-- Filtrar la imagen según el umbral AZUL:
+    for (var i = 0; i < data.length; i+=4) {
+      if (data[i] > umbral_B)
+        data[i] = umbral_B;
+    }
+  }
 
-    //-- Poner la imagen modificada en el canvas
+  function toGrey() {
+    for (var i = 0; i < data.length;i +=4) {
+      light = (3 * data[i] + 4 * data[i+1] + data[i+2])/8
+        data[i] = light;
+        data[i+1] = light;
+        data[i+2] = light;
+    }
+  }
+
+  //-- Funcion de retrollamada del deslizador ROJO:
+  deslizador_R.oninput = () => {
+    toRGB();
+    //-- Poner la imagen modificada en el canvas:
+    ctx.putImageData(imgData, 0, 0);
+  }
+  //-- Funcion de retrollamada del deslizador VERDE:
+  deslizador_G.oninput = () => {
+    toRGB();
+    //-- Poner la imagen modificada en el canvas:
+    ctx.putImageData(imgData, 0, 0);
+  }
+  //-- Funcion de retrollamada del deslizador AZUL:
+  deslizador_B.oninput = () => {
+    toRGB();
+    //-- Poner la imagen modificada en el canvas:
+    ctx.putImageData(imgData, 0, 0);
+  }
+
+  gris.onclick = () => {
+    toGrey();
+    //-- Poner la imagen modificada en el canvas:
     ctx.putImageData(imgData, 0, 0);
   }
 
